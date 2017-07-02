6df9833ed48ee1895b4c8269c327b1d2d90d7d2f
amalakamala <amalakamala.a@gmail.com>
2017-07-02 14:05:06 -0400

    validacion

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
 function validateForm(){
 	/* Escribe tú código aquí */
 }
+
+$(document).ready(function(){
+	$(".submit-btn").click(function(){
+		if(!(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]{2,13})+$/.test($("#name").val()))){
+   			$("#name").parent().append("<span>Ingrese Nombre correcto<br>primera letra mayúscula</span>");
+  		}
+
+  		if(!(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]{2,13})+$/.test($("#lastname").val()))){
+   			$("#lastname").parent().append("<span>Ingrese Apellido correcto<br>primera letra mayúscula</span>");
+  		}
+
+  		if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test($("#input-email").val())) ){
+   			$("#input-email").parent().append("<span>Ingrese correo valido</span>");
+  		}
+
+  		if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test($("#input-email").val())) ){
+   			$("#input-email").parent().append("<span>Ingrese correo valido</span>");
+  		}
+
+  		if(($("#input-password").val() == "password") || ($("#input-password").val() == 123456) || ($("#input-password").val() == 098754) || (($("#input-password").val()).length < 5)){
+  			$("#input-password").parent().append("<span>Ingrese contraseña valida</span>");
+  		}
+
+  		if($("select").val() === "0") {
+   			$("select").parent().append("<span>Elige una opción</span>");
+  		}	
+
+
+  		$("#name").val("");
+  		$("#lastname").val("");
+  		$("#input-email").val("");
+  		$("#input-password").val("");
+  		$("select").val("0");  
+  		$("#input-social").val(""); 
+  		$("input:checkbox").val(""); 
+  		$('input:checkbox[value=remember-me]').attr('checked',false);	
+  		$('input:checkbox[value=remember-me]').checkbox("");			  		  				  		
+	});
+});
